41471bc1c2286d46d21f07c453dfb41b61e56a35
whitia <whitia7@gmail.com>
2022-07-24 19:26:47 +0900

    Update index component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-const Index = () => {
+import axios from 'axios'
+import Link from 'next/link'
+
+type Recipe = {
+  id: number
+  title: string
+  path: string
+}
+
+export const getServerSideProps = async () => {
+  const response = await axios.get('http://localhost:3001/api/v1/recipes')
+  const data = response.data
+  return { props: { data } }
+}
+
+const Home = ({ data }: any) => {
+  const recipes: Recipe[] = data
   return (
-    <div>Hello world</div>
+    <ul>
+      {recipes.map((recipe: Recipe) => {
+        return (
+          <li key={recipe.id}>
+            <Link href={`/recipes/${encodeURIComponent(recipe.path)}`}>
+              <a>{recipe.title}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
-export default Index
+export default Home
