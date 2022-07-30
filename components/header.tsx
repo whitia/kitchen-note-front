3d4d5857e0a33aab4351023e41dced1ef9aabd66
whitia <whitia7@gmail.com>
2022-07-30 17:44:24 +0900

    Fix header design

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>
-      <Link href='/recipes'>
-        一覧
-      </Link>
-    </div>
+    <>
+      <div className="container mx-auto max-w-2xl flex justify-between items-end mt-3 mb-5">
+        <Link href='/recipes'>
+          <a className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            <span className="font-semibold">VIOLET</span>
+            <span className="font-extralight">ROSE</span>
+          </a>
+        </Link>
+        <Link href='/recipes/new'>
+          <a className="btn py-2 px-4 text-sm">新規作成</a>
+        </Link>
+      </div>
+    </>
   )
 }
 
