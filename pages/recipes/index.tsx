f8a797bfc5d6ba9949c6f6828f941609f4195d5f
whitia <whitia7@gmail.com>
2022-07-30 17:45:49 +0900

    Fix recipe index design

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -7,25 +7,39 @@ export const getStaticProps = async () => {
 }
 
 const Index = ({ data }: any) => {
-  const recipes: Recipe[] = data['data']
+  const recipes: Recipe[] = data['data']['recipes']
+  const image_urls: string[] = data['data']['image_urls']
   if (data['status'] == 'SUCCESS') {
     return (
-      <div>
-        <Link href='/recipes/new'>
-          <a>新規作成</a>
-        </Link>
-        <ul>
-          {recipes.map((recipe: Recipe) => {
+      <>
+        <div className="mb-2">
+          <Link href={`/recipes/${encodeURIComponent(recipes[0].uuid)}`}>
+            <a>
+              <img title={recipes[0].title} src={image_urls[0]} className="object-cover object-center mb-1 w-full h-[28rem] rounded-lg" />
+              {/* <div className="font-bold text-slate-700">
+                {recipes[0].title}
+              </div> */}
+            </a>
+          </Link>
+        </div>
+        <ul className="grid grid-cols-3 gap-2">
+          {recipes.map((recipe: Recipe, index: number) => {
+            if (index == 0) return
             return (
               <li key={recipe.id}>
                 <Link href={`/recipes/${encodeURIComponent(recipe.uuid)}`}>
-                  <a>{recipe.title}</a>
+                  <a>
+                    <img title={recipe.title} src={image_urls[index]} className="object-cover object-center w-full h-44 rounded-lg" />
+                    {/* <div className="font-bold text-slate-700">
+                      {recipe.title}
+                    </div> */}
+                  </a>
                 </Link>
               </li>
             )
-          })}
+          }).filter(e => e)}
         </ul>
-      </div>
+      </>
     )
   }
 }
