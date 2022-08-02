9a307bb0857e039c084df5ac40663b8c15c79dce
whitia <whitia7@gmail.com>
2022-08-03 07:23:41 +0900

    Fix app config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
 import Layout from '../components/layout'
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <ToastContainer />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
