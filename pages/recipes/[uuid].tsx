2eac3f5ba095331f11688610ea7773154bd37195
whitia <whitia7@gmail.com>
2022-08-03 07:23:17 +0900

    Fix show

--- pages/recipes/[uuid].tsx
+++ pages/recipes/[uuid].tsx
@@ -1,6 +1,79 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { toast, Theme } from 'react-toastify'
 import action from '../../lib/recipes/action'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUpRightFromSquare, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+const Recipe = ({ data }: any) => {
+  const recipe: Recipe = data['data']['recipe']
+  const image_url: string = data['data']['image_url']
+  const ingredients: string[] = data['data']['ingredients']
+
+  const router = useRouter()
+  const handleDelete = async (event: any, uuid: string) => {
+    event.preventDefault()
+
+    if (window.confirm('Do you really want to delete?')) {
+      const data = await action.destroy(uuid)
+      let mode: Theme = 'light'
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        mode = 'dark'
+      }
+      toast('🎉 Have just deleted item.', { theme: mode })
+      router.push('/recipes')
+    }
+  }
+
+  return (
+    <>
+      <img title={recipe.title} src={image_url} className="img-cover h-64 md:h-[28rem] mb-8" />
+      <div className="flex justify-between items-end mb-8">
+        <h2 className="text-3xl">{recipe.title}</h2>
+        <p>
+          <Link href={`/recipes/edit/${encodeURIComponent(recipe.uuid)}`}>
+            <a><FontAwesomeIcon icon={faPenToSquare} /></a>
+          </Link>
+          &nbsp;/&nbsp;
+          <a href="#" onClick={(event) => handleDelete(event, recipe.uuid)}>
+            <FontAwesomeIcon icon={faTrashCan} />
+          </a>
+        </p>
+      </div>
+      <div className="mb-8">
+        <h3 className="mb-2 font-bold">Ingredients</h3>
+        <ul className="flex flex-wrap">
+          {ingredients.map((ingredient: string, index: number) => {
+            return (
+              <li key={index} className="mr-3 my-3">
+                <Link href={{
+                  pathname: '/recipes/search',
+                  query: { query: encodeURIComponent(ingredient) },
+                }}>
+                  <a className="badge">
+                    {ingredient}
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="mb-8">
+        <h3 className="mb-2 font-bold">External Link</h3>
+        <p className="text-sm">
+          <a href={recipe.external_url} target="_blank" rel="noopener noreferrer">
+            {recipe.external_title}
+            &nbsp;
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Recipe
 
 export const getStaticPaths = async () => {
   const data = await action.index()
@@ -15,62 +88,3 @@ export const getStaticProps = async (params: any) => {
   const data = await action.show(uuid)
   return { props: { data } }
 }
-
-const Recipe = ({ data }: any) => {
-  const recipe: Recipe = data['data']['recipe']
-  const image_url: string = data['data']['image_url']
-  const ingredients: string[] = data['data']['ingredients']
-
-  const router = useRouter()
-  const handleDelete = async (event: any, uuid: string) => {
-    event.preventDefault()
-    const data = await action.destroy(uuid)
-
-    router.push('/recipes')
-  }
-
-  if (data['status'] == 'SUCCESS') {
-    return (
-      <>
-        <img title={recipe.title} src={image_url} className="object-cover object-center w-full h-[28rem] rounded-lg mb-8" />
-
-        <div className="flex justify-between items-end mb-8">
-          <h2 className="text-4xl font-semibold">{recipe.title}</h2>
-          <p>
-            <Link href={`/recipes/edit/${encodeURIComponent(recipe.uuid)}`}>
-              <a>編集</a>
-            </Link>
-            &nbsp;/&nbsp;
-            <a href="#" onClick={(event) => handleDelete(event, recipe.uuid)}>
-              削除
-            </a>
-          </p>
-        </div>
-
-        <div className="mb-8">
-          <h3 className="text-2xl font-semibold mb-2">材料</h3>
-          <ul className="flex flex-wrap">
-            {ingredients.map((ingredient: string, index: number) => {
-              return (
-                <li key={index} className="mr-3 my-3">
-                  <Link href={`/recipes`}>
-                    <a className="p-2 text-sm text-slate-700 bg-slate-100 rounded-sm">
-                      {ingredient}
-                    </a>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-
-        <div className="mb-8">
-          <h3 className="text-2xl font-semibold mb-2">レシピ</h3>
-          <p><a href={recipe.external_url} target="_blank" rel="noopener noreferrer">{recipe.external_title}</a></p>
-        </div>
-      </>
-    )
-  }
-}
-
-export default Recipe
