c964af9361228c1f760a28e4e6481b4e682c820a
whitia <whitia7@gmail.com>
2022-08-03 07:20:14 +0900

    Add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="container mx-auto max-w-2xl mt-8 py-4 text-center">
+        <span className="text-neutral-400 text-xs dark:text-neutral-600">&copy; 2022 AUTOVICE</span>
+      </div>
+    </>
+  )
+}
+
+export default Footer
