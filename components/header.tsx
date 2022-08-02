f4d5052d7f052e3db9c9ba0b86a60199bd8a218d
whitia <whitia7@gmail.com>
2022-08-03 07:16:59 +0900

    Add environment and fix site title on header

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 
 const Header = () => {
+  const title = process.env.NEXT_PUBLIC_SITE_TITLE
+  const titleSplit = title?.split(' ')
   return (
     <>
       <div className="container mx-auto max-w-2xl flex justify-between items-end mt-3 mb-5">
         <Link href='/recipes'>
           <a className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-            <span className="font-semibold">VIOLET</span>
-            <span className="font-extralight">ROSE</span>
+            <span className="font-semibold">{titleSplit && titleSplit[0]}</span>
+            <span className="font-extralight">{titleSplit && titleSplit[1]}</span>
           </a>
         </Link>
         <Link href='/recipes/new'>
