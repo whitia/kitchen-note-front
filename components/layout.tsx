de5c2ba06c78c8188debd203f89765814e38fc33
whitia <whitia7@gmail.com>
2022-08-03 07:22:54 +0900

    Fix layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,22 @@
+import Head from 'next/head'
 import Header from './header'
+import Footer from './footer'
 
 const Layout = ({children}: any) => {
+  let title = process.env.NEXT_PUBLIC_SITE_TITLE
+  if (children.props['data']) {
+    const recipe = children.props['data']['data']['recipe']
+    if (recipe) title = recipe['title'] + ' | ' + title
+  }
   return (
     <>
-      <Header />
-      <div className="container mx-auto max-w-2xl">
-        <main>{children}</main>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="flex flex-col h-screen justify-between px-4 md:px-0">
+        <Header />
+        <main className="container mx-auto max-w-full md:max-w-2xl mb-auto">{children}</main>
+        <Footer />
       </div>
     </>
   )
