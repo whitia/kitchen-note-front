ebcaf1ccced6121c67f3f44e612d4afb0583f2e3
whitia <whitia7@gmail.com>
2022-08-03 09:54:47 +0900

    Add search icon

--- components/search.tsx
+++ components/search.tsx
@@ -1,21 +1,28 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useRouter } from 'next/router'
+import { useRef } from 'react'
 
 const Search = () => {
   const router = useRouter()
+  const inputSearch = useRef<HTMLInputElement>(null)
   const handleSearchSubmit = async (event: any) => {
     event.preventDefault()
 
-    const query = event.target.search.value
-    router.push({
-      pathname: '/recipes/search',
-      query: { query: encodeURI(query) }
-    })
+    if (inputSearch.current?.value) {
+      const query = inputSearch.current?.value
+      router.push({
+        pathname: '/recipes/search',
+        query: { query: encodeURI(query) }
+      })
+    }
   }
 
   return (
     <>
-      <form onSubmit={handleSearchSubmit}>
-        <input className="input-text m-0 px-4 py-2 rounded-full" type="text" id="search" name="search" placeholder="Search by ingredient" />
+      <form onSubmit={handleSearchSubmit} className="relative">
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute top-3.5 left-4 cursor-pointer" onClick={handleSearchSubmit} />
+        <input ref={inputSearch} className="input-text m-0 pl-10 pr-4 py-2 rounded-full" type="text" id="search" name="search" placeholder="Search by ingredient" />
       </form>
     </>
   )
