35894740318685bf2c9e13e977fefdee9e92a044
whitia <whitia7@gmail.com>
2022-08-07 15:02:24 +0900

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
+  let title = process.env.NEXT_PUBLIC_SITE_TITLE
   return (
     <>
       <div className="container mx-auto max-w-2xl mt-8 py-4 text-center">
-        <span className="text-neutral-400 text-xs dark:text-neutral-600">&copy; 2022 AUTOVICE</span>
+        <span className="text-neutral-400 text-xs dark:text-neutral-600">
+          &copy; 2022 {title}
+        </span>
       </div>
     </>
   )
